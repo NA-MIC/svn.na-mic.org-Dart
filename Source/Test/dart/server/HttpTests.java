622046809b363f987d37529e1d582b72bfaba9bf
Amitha Perera <amitha.perera@kitware.com>
2006-01-06 21:05:08 +0000

    Use HTTPUnit to test webpage contents. Only one test is more detailed for the moment. Add appropriate licenses to the manual.
    git-svn-id: http://svn.na-mic.org/Dart/trunk@253 b2c14c61-d7ef-0310-8715-841e9ab387fe

--- Source/Test/dart/server/HttpTests.java
+++ Source/Test/dart/server/HttpTests.java
@@ -10,6 +10,8 @@ import dart.*;
 import dart.server.*;
 import dart.server.task.*;
 import java.net.*;
+import com.meterware.httpunit.*;
+import java.util.regex.*;
 
 public class HttpTests extends TestCase {
 
@@ -17,17 +19,43 @@ public class HttpTests extends TestCase {
     super ( s );
   }
   Project project;
+  WebConversation webConv;
 
   public void setUp() {
     project = DartServerTest.getProject();
+    webConv = new WebConversation();
   }
 
+  /** Verify that the webpage exists
+   *
+   * Does not verify the contents of the page in any way.
+   */
   boolean hasContent ( String u ) throws Exception {
     URL url = new URL ( u );
     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
     conn.connect();
     return ( conn.getResponseCode() == HttpURLConnection.HTTP_OK );
   }
+
+  /** Verify that the webpage contains all the regexps provided.
+   *
+   * The regexps are matched using "find", not "matches", so that only
+   * a substring within the webpage need match the regexp.
+   */
+  void assertPageContains( String url, String[] regexps ) throws Exception {
+    WebResponse resp = webConv.getResponse( url );
+    String webtext = resp.getText();
+    java.lang.System.out.println( "Text: "+webtext );
+    for( int i = 0; i < regexps.length; ++i ) {
+      Pattern p = Pattern.compile( regexps[i] );
+      Matcher m = p.matcher( webtext );
+      boolean r = m.find();
+      java.lang.System.out.println( "Find: "+regexps[i]+": "+r );
+      assertTrue( r );
+    }
+  }
+      
+
 
   public void testPingServer () throws Exception {
     assertTrue ( hasContent ( "http://localhost:" + DartServerTest.PortNumber + "/" + DartServerTest.projectName + "/Dashboard" ) );
@@ -37,8 +65,17 @@ public class HttpTests extends TestCase {
     assertTrue ( hasContent ( "http://localhost:" + DartServerTest.PortNumber + "/" + DartServerTest.projectName + "/Dashboard/Submission?submissionid=1" ) );
   }
 
-  public void testTestDetailPage () throws Exception {
-    assertTrue ( hasContent ( "http://localhost:" + DartServerTest.PortNumber + "/" + DartServerTest.projectName + "/Dashboard/Test?testname=.Test.Testing.Code.Common.PrintSelf-Common&submissionid=1" ) );
+  public void testTestDetailPage() throws Exception {
+    String[] contentRegExps =
+      {
+        "Insight/Code/Common",           // Test output is displayed
+        "25.107994",     // Execution time is displayed
+        "Completed",  // Completion status is displayed
+        "Site Name:.*caleb\\.crd",       // Site name is displayed
+        "Build Name:.*SunOS-c\\+\\+"     // Build name is displayed
+      };
+    assertPageContains( "http://localhost:" + DartServerTest.PortNumber + "/" + DartServerTest.projectName + "/Dashboard/Test?testname=.Test.Testing.Code.Common.PrintSelf-Common&submissionid=1",
+                         contentRegExps );
   }
 
   public void testChart () throws Exception {
@@ -48,6 +85,8 @@ public class HttpTests extends TestCase {
   public void testNonexistentSubmissionOverview () throws Exception {
     assertFalse ( hasContent ( "http://localhost:" + DartServerTest.PortNumber + "/" + DartServerTest.projectName + "/Dashboard/Submission?submissionid=165443" ) );
   }
+
+    
 
   public static Test suite() {
     TestSuite tests = new TestSuite();
