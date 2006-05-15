3848b85664eb12359efb03f7cf377fd4e66b7ee3
Daniel Blezek <Blezek.Daniel@mayo.edu>
2006-05-15 19:05:59 +0000

    Added SVN version info to Version.java
    git-svn-id: http://svn.na-mic.org/Dart/trunk@376 b2c14c61-d7ef-0310-8715-841e9ab387fe

--- Source/dart/server/Version.java
+++ Source/dart/server/Version.java
@@ -4,11 +4,11 @@ package dart.server;
 
 public class Version {
   // Contains Version info for Dart.
-  public static final int MajorVersion = 0;
-  public static final int MinorVersion = 9;
+  public static final int MajorVersion = 1;
+  public static final int MinorVersion = 0;
   public static final int PatchVersion = 0;
   public static String getVersionString() {
-    return MajorVersion + "." + MinorVersion + "." + PatchVersion;
+    return MajorVersion + "." + MinorVersion + "." + PatchVersion + ": $Id$";
   }
   static public String getDBVersionString() {
     return Project.getDBVersionString();
